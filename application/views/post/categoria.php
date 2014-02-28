15d2034d44718d1b898375c79bb19ead17e9a828
brito89 <cocobrito3@gmail.com>
2014-02-27 20:18:16 -0600

    Revert "initial Commit"
    This reverts commit bd05a6b3093b4954b2408825f11d86c4bb13dc11.

--- application/views/post/categoria.php
+++
@@ -1,9 +0,0 @@
-<select name="categoria" id="categoria" class="result-categoria">
-    <option value="">Seleccione una Categoría</option>
-    <?php
-        foreach($categorias->result() as $categoria)
-        {
-            echo '<option value="'.$categoria->idCategorias.'">'.$categoria->nombre.'</option>';
-        }
-    ?>
-</select>
--- application/views/subcategoria.php
+++
@@ -1,9 +0,0 @@
-<select name="subcategoria" id="subcategoria">
-   <option value="">Seleccione una Sub-Categoría</option>
-   <?php
-   		foreach($subcategorias->result() as $subcategoria)
-		{
-			echo '<option value="'.$subcategoria->idSubcategorias.'">'.$subcategoria->nombre.'</option>';
-		}
-   ?>
-</select>
